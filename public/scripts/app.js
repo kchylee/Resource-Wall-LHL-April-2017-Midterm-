b1d233f6f7b78d28be3dcf5aea3caeac96a10f17
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-27 14:08:25 -0400

    Started creating the route to insert a new resource. Touched app.js to add the function and resources.js to add the route.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,9 @@
+
+// jquery document ready
 $(() => {
+
+  // ajax call to api/users. Adds DIVs to index.ejs
+  // This example csn be used later to load the home page.
   $.ajax({
     method: "GET",
     url: "/api/users"
@@ -6,5 +11,42 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.first_name).appendTo($("body"));
     }
-  });;
+  });
+
+  // Resource functions.
+  const getResources = () => {
+    console.log("I'm a placeholder for a function.");
+  }
+
+  const insertResource = (formData) => {
+    $.ajax({
+      method: "POST",
+      url: "/api/resources",
+      data: formData,
+      success: function (data, status, obj) {
+        console.log("status: ", status);
+        console.log("object: ", obj);
+        console.log("data: ", data);
+        },
+        error: function (obj1, e, obj2) {
+          console.log(e);
+        }
+
+    });
+  }
+
+  // Insert resource. Catches the event from a form and inserts data in the DB
+  let $insertResourceForm = $('#insert-resource');
+  $insertResourceForm.on("submit" (event) => {
+    event.preventDefault();
+    //validate content here: URL, title and description.
+
+
+    //All good after validation? then invoke the insertion, pass the form data.
+    insertResource($(this).serialize());
+  })  // ends insertResourceForm.on
+
+
+
+// Ends jquery document ready.
 });
--- server.js
+++ server.js
@@ -16,11 +16,12 @@ const knexLogger  = require('knex-logger');
 
 // Separated Routes for each Resource
 const usersRoutes = require("./routes/users");
-
+const resourcesRoutes = require("./routes/resources");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes,
+//         and uncolored for all other codes.
 app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
@@ -38,6 +39,7 @@ app.use(express.static("public"));
 
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
+app.use("/api/resources", resourcesRoutes(knex));
 
 // Home page
 app.get("/", (req, res) => {
